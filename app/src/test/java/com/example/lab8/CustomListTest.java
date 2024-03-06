16d932d8f5787af439e04a5b5754fc2b53328fb2
uabaz <azwarich@ualberta.ca>
2024-03-06 12:26:07 -0700

    New add city test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,48 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
+    /**
+     * create a mocklist for my citylist
+     * @return
+     */
 
+    public CustomList MockCityList(){
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+
+    /**
+     * get the size of the list
+     * @return
+     */
+
+    public int getCount(){
+        return cities.size();
+    }
+
+    /**
+     * add a city object to the list
+     * for the first phase it will be empty
+     * @param city
+     *
+     */
+    public void addCity(City city){
+
+    }
+
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size+1
+     */
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(), listSize+1);
+    }
 }
+
